13dbf44313d4ccfab04c7a7f56efbb60fde0fb2c
SecH0us3 <github@ioc.slmail.me>
2026-06-16 20:15:17 +0300

    chore: remove scratch scripts from git tracking and add scratch/ to gitignore

--- scratch/fix_bola.py
+++
@@ -1,7 +0,0 @@
-with open("/Users/alex/src/swazz/packages/container/internal/runner/bola.go", "r") as f:
-    code = f.read()
-
-code = code.replace("var resolvedPath string\n\t\tvar harvested []string", "var harvested []string")
-with open("/Users/alex/src/swazz/packages/container/internal/runner/bola.go", "w") as f:
-    f.write(code)
-print("Fixed declared and not used.")
--- scratch/limits.py
+++
@@ -1,9 +0,0 @@
-with open("/Users/alex/src/swazz/packages/container/internal/runner/bola.go", "r") as f:
-    code = f.read()
-
-code = code.replace("if limit > 5 {\n\t\t\tlimit = 5 // try up to 5 harvested IDs\n\t\t}", "if limit > 25 {\n\t\t\tlimit = 25 // brute force up to 25 harvested IDs\n\t\t}")
-code = code.replace("if limit > 3 {\n\t\t\t\t\tlimit = 3\n\t\t\t\t}", "if limit > 25 {\n\t\t\t\t\tlimit = 25\n\t\t\t\t}")
-
-with open("/Users/alex/src/swazz/packages/container/internal/runner/bola.go", "w") as f:
-    f.write(code)
-print("Updated limits.")
