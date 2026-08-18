073cb943378808bafb61fb1206485dfbc1b26d02
SecH0us3 <github@ioc.slmail.me>
2026-08-18 22:10:22 +0300

    chore: remove tracked storageState.json and scratch files

--- scratch.js
+++
@@ -1,3 +0,0 @@
-const fs = require('fs');
-const glob = require('glob'); // maybe not available? Use fs.readdirSync recursively
-// Actually, let's just use sed for waitForTimeout and registerAndLogin
