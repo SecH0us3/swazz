1ca258ac53e8753de73088a9e1714885ac285832
Alex P <github@ioc.slmail.me>
2026-08-24 08:43:49 +0300

    feat: add automated test coverage badges (#651)
    feat: add automated test coverage badges

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -13,6 +13,9 @@ export default defineConfig({
         globals: true,
         environment: 'jsdom',
         setupFiles: ['./src/vitest.setup.ts'],
+        coverage: {
+            reporter: ['text', 'json-summary']
+        }
     },
     server: {
         port: 5173,
